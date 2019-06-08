387062366df81a52553d21aa7933861d5e4386c7
TiagoFueber <tfuelber@gmail.com>
2019-06-08 16:48:22 -0300

    Initial HTML static structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
-const App = () => <h1>Search books</h1>;
+const App = () => (
+  <>
+    <header>
+      <h1>Search books</h1>
+      <div>
+        <input type="search" />
+        <label htmlFor="favourites">
+          <input name="favourites" type="checkbox" />
+          Favourites
+        </label>
+      </div>
+    </header>
+    <div className="container">
+      <div className="book">Book 1</div>
+      <div className="book">Book 2</div>
+      <div className="book">Book 3</div>
+      <div className="book">Book 4</div>
+      <button>Load more</button>
+    </div>
+  </>
+);
 
 export default App;
